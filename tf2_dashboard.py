8d1bbcce65c9e6247925088b2f78acd1d9d40ee1
unknown <chuckcclift@gmail.com>
2020-10-02 23:07:00 -0400

    removed the weapon deaths column because the information is redundant.  The info is too specific to be useful.  Damage type deaths and class deaths are more useful

--- tf2_dashboard.py
+++ tf2_dashboard.py
@@ -43,9 +43,9 @@ tk.Label(master, text="stats", width=10, anchor="e").grid(row=1, column=1, stick
 tk.Label(master, text="victims", width=15, anchor="e").grid(row=1, column=2)
 tk.Label(master, text="rivals", width=15, anchor="e").grid(row=1, column=3, sticky=tk.E)
 tk.Label(master, text="", width=3, anchor="e").grid(row=1, column=4, sticky=tk.E)
-tk.Label(master, text="weapons", width=15, anchor="e").grid(row=1, column=5, sticky=tk.E)
-tk.Label(master, text="classes", width=15, anchor="e").grid(row=1, column=6, sticky=tk.E)
-tk.Label(master, text="dmg type  ", width=15, anchor="e").grid(row=1, column=7, sticky=tk.E)
+# tk.Label(master, text="weapons", width=15, anchor="e").grid(row=1, column=5, sticky=tk.E)
+tk.Label(master, text="classes", width=15, anchor="e").grid(row=1, column=5, sticky=tk.E)
+tk.Label(master, text="dmg type  ", width=15, anchor="e").grid(row=1, column=6, sticky=tk.E)
 
 tk_ize = lambda c: ImageTk.PhotoImage(Image.open(f"tf2_icons/{c}.png").resize((16,16)))
 class_tk_imgs = {cn: tk_ize(cn) for cn in class_icons}
@@ -59,7 +59,7 @@ tk.Label(master, textvariable=tk_elo).grid(row=5, column=1, sticky=tk.E)
 
 victim_vars = [tk.StringVar() for _ in range(5)]
 rival_vars = [tk.StringVar() for _ in range(5)]
-rival_weapon_vars = [tk.StringVar() for _ in range(5)]
+# rival_weapon_vars = [tk.StringVar() for _ in range(5)]
 rival_class_vars = [tk.StringVar() for _ in range(5)]
 rival_dmg_type_vars = [tk.StringVar() for _ in range(5)]
 
@@ -73,9 +73,9 @@ for i, r in zip(range(2, 7), rival_class_icons):
 for i in range(2, 7):
     tk.Label(master, textvariable=victim_vars[i-2]).grid(row=i, column=2, sticky=tk.E)
     tk.Label(master, textvariable=rival_vars[i-2]).grid(row=i, column=3, sticky=tk.E)    
-    tk.Label(master, textvariable=rival_weapon_vars[i-2] ).grid(row=i, column=5, sticky=tk.E)
-    tk.Label(master, textvariable=rival_class_vars[i-2]).grid(row=i, column=6, sticky=tk.E)
-    tk.Label(master, textvariable=rival_dmg_type_vars[i-2]).grid(row=i, column=7, sticky=tk.E)
+    # tk.Label(master, textvariable=rival_weapon_vars[i-2] ).grid(row=i, column=5, sticky=tk.E)
+    tk.Label(master, textvariable=rival_class_vars[i-2]).grid(row=i, column=5, sticky=tk.E)
+    tk.Label(master, textvariable=rival_dmg_type_vars[i-2]).grid(row=i, column=6, sticky=tk.E)
 
 def calculate_elo(killer_elo, victim_elo, k=30):
     killer_expected_score = 1 / (1 + 10 ** ( (victim_elo - killer_elo) / 400))
@@ -143,7 +143,6 @@ def render() -> None:
 
     kills = 0
     streak_kills = 0
-    # deaths = 0
     gamer_names = set()
     with open(fpath, encoding="utf-8", errors="ignore") as f:
         lines = f.readlines()
@@ -154,7 +153,6 @@ def render() -> None:
     server = ("", 0)
     while True:
         time.sleep(3)
-        weapon_deaths: Dict = defaultdict(lambda: 0)
         victims: Dict = defaultdict(lambda: 0)
         rivals: Dict = defaultdict(lambda: 0)
         class_deaths: Dict = defaultdict(lambda: 0)
@@ -182,15 +180,7 @@ def render() -> None:
         gamer_names.update(read_connections(game_lines))
         kill_events = [a for a in kill_events if a]
 
-        player_elo: Dict[str, float] = defaultdict(lambda: 1600.0)
-        for killer, victim, weapon in kill_events:
-            (k_elo, v_elo) = calculate_elo(player_elo[killer], player_elo[victim])
-            player_elo[killer] = k_elo
-            player_elo[victim] = v_elo
-
-        
-
-            
+        player_elo: Dict[str, float] = defaultdict(lambda: 1600.0)        
         kills = sum(1 for k, _,_ in kill_events if k == user)
         deaths: int = len([line for line in game_lines
                           if f"killed {user}" in line or line == f"{user} died."])
@@ -214,7 +204,6 @@ def render() -> None:
                 victims[victim] += 1
             elif victim == user:
                 rivals[killer] += 1
-                weapon_deaths[weapon] += 1
                 class_deaths[weapon_class.get(weapon, "many")] += 1
                 dmg_type_deaths[weapon_dmg.get(weapon, "melee")] += 1
 
@@ -227,16 +216,14 @@ def render() -> None:
         tk_streak.set(f"streak: {streak_kills}")
         tk_elo.set(f"elo: {player_elo[user]:.1f}")
         
+        
         # sorting columns into descending order
         for (vkills, vname), vv in zip(sort_descending(victims), victim_vars ):
             vv.set(f"{vname}: {vkills}")
 
         for (rdeaths, rname), rv, rci in zip(sort_descending(rivals), rival_vars, rival_class_icons):
-            rv.set(f"{rname} ({player_elo[rname]:.1f} ) : {rdeaths}")              # {ci(rname)}
+            rv.set(f"{rname} ({player_elo[rname]:.1f} ) : {rdeaths}")
             rci.configure(image=class_tk_imgs[player_class.get(rname, "many")])
-
-        for (wdeaths, wname), wv in zip(sort_descending(weapon_deaths), rival_weapon_vars):
-            wv.set(f"{wname}: {wdeaths}")
 
         for (cdeaths, cname), rcv in zip(sort_descending(class_deaths), rival_class_vars):
             rcv.set(f"{cname}: {cdeaths}")
